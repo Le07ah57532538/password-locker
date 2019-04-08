09bf3943b66ab5a1830c25818f61af49d4533e3a
leah <leahgakii74@gmail.com>
2019-04-08 12:05:55 +0300

    self, a variable that represents instance of an object itself

--- user.py
+++ user.py
@@ -5,14 +5,14 @@ class User:
    user_list = [] #Empty user list
 
 #    def __init__(self,first_name , last_name):
-#        '''
-#        init method to create instances of the user class
-#        Args:
-#            first_name: New User.first_name
-#            last_name: New user.last_name
-#        '''
-#        self.first_name = first_name
-#        self.last_name = last_name
+       '''
+       init method to create instances of the user class
+       Args:
+           first_name: New User.first_name
+           last_name: New user.last_name
+       '''
+       self.first_name = first_name
+       self.last_name = last_name
 
    def save_user(self):
        '''
