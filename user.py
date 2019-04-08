5a2eac52e2cb35425ba886d75a38fda4de50133a
leah <leahgakii74@gmail.com>
2019-04-08 12:01:11 +0300

    _init_ method introduced to allow us to create new instances

--- user.py
+++ user.py
@@ -1,18 +1,18 @@
-# class User:
-#    '''
-#    Class that generates new instances of users
-#    '''
+class User:
+   '''
+   Class that generates new instances of users
+   '''
    user_list = [] #Empty user list
 
-   def __init__(self,first_name , last_name):
-       '''
-       init method to create instances of the user class
-       Args:
-           first_name: New User.first_name
-           last_name: New user.last_name
-       '''
-       self.first_name = first_name
-       self.last_name = last_name
+#    def __init__(self,first_name , last_name):
+#        '''
+#        init method to create instances of the user class
+#        Args:
+#            first_name: New User.first_name
+#            last_name: New user.last_name
+#        '''
+#        self.first_name = first_name
+#        self.last_name = last_name
 
    def save_user(self):
        '''
