f7e7aefed0a9aaae3dc052aa8ec260418514ac89
leah <leahgakii74@gmail.com>
2019-04-08 11:59:08 +0300

    created a class to generate a docstring for it.

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
-class User:
-   '''
-   Class that generates new instances of users
-   '''
+# class User:
+#    '''
+#    Class that generates new instances of users
+#    '''
    user_list = [] #Empty user list
 
    def __init__(self,first_name , last_name):
